fa2c51d3b3fff0033fcbf572e5ae7bcc11e790f4
birbalkr <birbalkr1435@gmail.com>
2026-07-27 22:02:12 +0530

    Build failed: fix file name import

--- src/components/ExpenseInput.jsx
+++ src/components/ExpenseInput.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
-import { addExpense } from '../redux/features/ExpenseTrackerSlice';
+import { addExpense } from '../redux/features/expenseTrackerSlice';
 import { nanoid } from '@reduxjs/toolkit';
 
 function ExpenseInput() {
