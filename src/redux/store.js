8af915124db8b5d3b1b34ea6e22202d734d86dc6
birbalkr <birbalkr1435@gmail.com>
2026-07-27 21:58:36 +0530

    fix hosting error import store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import expensesTrackerReducer from './features/ExpenseTrackerSlice'
+import expensesTrackerReducer from './features/expenseTrackerSlice'
 
 export const store = configureStore({
     reducer: {
